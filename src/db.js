51cfb5e47976fd7e1820360c7a7cd1f68bb835f7
Sami2703 <samikshasapat03@gmail.com>
2025-12-31 16:19:16 +0530

    Fix MongoDB connection for Vercel

--- src/db.js
+++ src/db.js
@@ -1,8 +1,23 @@
 const mongoose = require("mongoose");
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 async function connectDB() {
-  await mongoose.connect(process.env.MONGODB_URI);
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+      bufferCommands: false
+    });
+  }
+
+  cached.conn = await cached.promise;
   console.log("MongoDB connected");
+  return cached.conn;
 }
 
 module.exports = connectDB;
