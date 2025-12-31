6d030555684d8e5c934fcd821f18cbd69ee0a436
Sami2703 <samikshasapat03@gmail.com>
2025-12-31 14:41:06 +0530

    all done

--- index.js
+++ index.js
@@ -7,16 +7,19 @@ const pastes = require("./routes/pastes");
 const view = require("./routes/view");
 const ui = require("./routes/Ui");
 
-
 const app = express();
 
+// Connect Database
 connectDB();
 
+// Middlewares
 app.use(express.json());
 
+// Routes
 app.use("/api", health);
 app.use("/api/pastes", pastes);
 app.use("/p", view);
 app.use("/", ui);
 
+// Export app for Vercel
 module.exports = app;
